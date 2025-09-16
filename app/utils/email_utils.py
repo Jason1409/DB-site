064f8afde4512ca165349f44eb74ea516f0642da
Jason1409 <raptornation0914@gmail.com>
2025-09-16 20:53:06 +0530

    Changed Email

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -5,7 +5,7 @@ from app.models.contacts import ContactForm
 EMAIL_USERNAME = os.getenv("EMAIL_USERNAME")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_FROM = os.getenv("EMAIL_FROM")
-EMAIL_TO = os.getenv("EMAIL_TO")
+EMAIL_TO = os.getenv("EMAIL_TO","")
 
 def send_contact_email(contact: ContactForm):
     subject = f"📩 New Contact Form Submission from {contact.name}"
@@ -18,11 +18,13 @@ Phone: {contact.phone}
 Message:
 {contact.message}
 """
+    recipients = [email.strip() for email in EMAIL_TO.split(",") if email.strip()]
+
 
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = EMAIL_FROM
-    msg["To"] = EMAIL_TO
+    msg["To"] = ", ".join(recipients)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(EMAIL_USERNAME, EMAIL_PASSWORD)
