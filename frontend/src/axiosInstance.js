3c7bb04db95b10be4922370858b09fb099337069
Jason1409 <raptornation0914@gmail.com>
2025-11-09 23:59:00 +0530

    Fix axios baseURL env variable

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set up your base URL — change this if you deploy to production
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL||"http://127.0.0.1:8000", // or "http://localhost:8000"
+  baseURL: import.meta.env.VITE_API_URL, // or "http://localhost:8000"
   headers: {
     "Content-Type": "application/json",
   },
